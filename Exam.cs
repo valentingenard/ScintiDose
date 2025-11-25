f1a461ad8aaecf6325d21c7ed007771cbaebc02d
Valentin Genard <valentingenard@gmail.com>
2025-11-25 21:06:43 +0100

    Adding Exam class.

---
+++ Exam.cs
@@ -0,0 +1,25 @@
+﻿internal class Exam
+{
+    public string Type { get; }
+    public double Weight { get; }
+
+    public Exam(string type, double weight)
+    {
+        Type = type.ToLower();
+        Weight = weight;
+    }
+
+    public double Activity()
+    {
+        if (Type.Contains("coeur"))
+            return (1.25 * (Weight - 70) + 720) / 37.0;
+
+        if (Type.Contains("osseuse"))
+            return (1.8 * (Weight - 70) + 740) / 37.0;
+
+        if (Type.Contains("ventriculographie"))
+            return 25.0;
+
+        return 0;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
-            DecayCalc decay = new DecayCalc("09:30", "07:30", 20);
-            Console.WriteLine(decay.Operate());
+            Exam calculExamen = new Exam("coeur", 75);
+            Console.WriteLine($"l'activité que le patient dois recevoir: {calculExamen.Activity()}");
+            
+            DecayCalc decay = new DecayCalc("09:30", "07:30", calculExamen.Activity());
+            Console.WriteLine($"L'activité à préparer à 7h30 pour qu'il ait  {calculExamen.Activity()} à 9h30: {decay.Operate()}");
+
             Console.ReadKey();
         }
     }
